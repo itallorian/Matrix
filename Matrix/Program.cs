ad3aecfbe25462249144d836d9f37e0f8b0c9ba2
Ítallo Rian <me@itallorian.com>
2022-07-10 16:58:50 -0300

    Add project files.

---
+++ Matrix/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.ForegroundColor = ConsoleColor.Green;
+
+Random random = new();
+string text = string.Empty;
+
+for(int index = 0; index < 20000; index++)
+{
+    if((index % 2) == 0)
+    {
+        text = string.Empty;
+
+        for(int subIndex = 0; subIndex < 79; subIndex++)
+        {
+            int number = random.Next(5);
+
+            if(number < 2)
+            {
+                text += number.ToString();
+                continue;
+            }
+
+            text += " ";
+        }
+
+        Console.WriteLine(text);
+    }
+}
